edc8bad345af0bae6769b3db0fde13dbcda08575
fumaf <fuma19981201.01@gmail.com>
2020-04-11 13:31:49 +0900

    Top　編集

--- src/TopScreen.js
+++ src/TopScreen.js
@@ -1,30 +1,117 @@
 import React ,{ useState }from 'react';
 import { StyleSheet, Text, View ,ScrollView,Button,TextInput,KeyboardAvoidingView} from 'react-native';
 import Modal from 'react-native-modalbox';
-import TopScreen from './src/TopScreen';
 
 
+export default function TopScreen() {
+  const [addModal, setAddModal] = useState(false);
 
-export default function App() {
+  const openModal = () => {
+    setAddModal(true);
+  };
+  
+  const closeModal = () => {
+    setAddModal(false);
+  };
 
   return (
-    <>
-      <TopScreen />
-    </>
+    <View style={styles.container}>
+      <View style={styles.checkBoxes}>
+        <View style={styles.namakemonoCheckBoxShadow}/>
+        <View style={styles.namakemonoCheckBox}>
+        </View>
+        <View style={styles.doneCheckBoxShadow}/>
+        <View style={styles.doneCheckBox}>
+        </View>
+      </View>
+      <View style={styles.imageBox}>
+        <View style={styles.imageShadow}/>
+        <View style={styles.image}>
+        </View>
+      </View>
+      <View style={styles.brake}>
+        <Text style={styles.bgText}>Today's Task</Text>
+        <View style={styles.taskAddButtonShadow}/>
+        <Button style={styles.taskAddButton} onPress={openModal} title="aa">
+      
+        </Button>
+      </View>
+      <Modal
+     style={styles.taskAddScreen}
+    //  ref={this.modalRef}
+     isOpen={addModal}
+     onClosed={closeModal}
+     >
+     
+        <TextInput autoFocus={true} style={styles.taskInput} placeholder='タスクを入力してください。'>
+        </TextInput>
+   </Modal>
+      <ScrollView showsVerticalScrollIndicator={false} >
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+        <Text>aaa</Text>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'relative',
-    flex: 1,
-    backgroundColor: '#F1F3F6',
-    alignItems: 'center',
-    paddingTop:'15%',
-    // justifyContent: 'center',
-        // backgroundColor: '#ddd',
+    container: {
+        position: 'relative',
+        flex: 1,
+        backgroundColor: '#F1F3F6',
+        alignItems: 'center',
+        paddingTop:'15%',
+        // justifyContent: 'center',
+            // backgroundColor: '#ddd',
+    
+      },
 
-  },
   checkBoxes: {
     height:'12%',
     // flex: 0.2,
